5968aa49ea3f6ba74b0fc5a8deac7621b576f486
BenjaPalazzo <benjapalazzo2@gmail.com>
2026-04-13 23:03:40 -0300

    Agrego response y coordenadas

--- satelite.py
+++ satelite.py
@@ -1,3 +1,6 @@
+import datetime
+import json
+
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 from dotenv import load_dotenv
 import os
@@ -6,23 +9,54 @@ load_dotenv()
 application = ApplicationBuilder().token(os.getenv("TOKEN")).build()
 
 async def message(update, context):
-    await update.message.reply_text("Como estas?")
+    await update.message.reply_text("📡 Bot de análisis satelital \n Enviá tu ubicación para analizar deformación del terreno")
     return
 def calcular_deformacion(lat, lon):
     return lat * 0.1 + lon * 0.05
 
 async def ubication(update,context):
+    delta = 0.01
     location = update.message.location
     lat = location.latitude
     lon = location.longitude
-    deformacion = calcular_deformacion(lat, lon) 
-    await update.message.reply_text(f"Lat: {lat}, Lon: {lon}, Def: {deformacion}")
+    if -90 <= lat <= 90 and -180 <= lon <= 180: 
+        north = lat + delta
+        south = lat - delta
+        west = lon - delta
+        east = lon + delta
+        start = datetime.datetime.now().strftime("%Y-%m-%d")
+        response = {
+            "azimuth_looks": None,
+            "connections": None,
+            "path": None,
+            "range_looks": None,
+            "sensor": None,
+            "workflow": None,
+            "east": east,
+            "north": north,
+            "south": south,
+            "west": west,
+            "end": "",
+            "start": start
+            }
+        await update.message.reply_text(f"{json.dumps(response)}")
+    else:
+        await update.message.reply_text("Datos invalidos!")
+    return 
+
+async def state(update, context):
+    await update.message.reply_text("Procesando")
     return
 
-
-ubicationHandle = MessageHandler(filters.LOCATION, ubication)
+ubicationHandler = MessageHandler(filters.LOCATION, ubication)
+stateHandler = CommandHandler("state", state)
 handler = MessageHandler(filters.TEXT, message)
+
+
+application.add_handler(stateHandler)
 application.add_handler(handler)
-application.add_handler(ubicationHandle)
+application.add_handler(ubicationHandler)
+
 application.run_polling()
+
 
